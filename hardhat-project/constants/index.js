96d60d55a14466718a2c3f6c8cdff4bb782511cb
sconkfinkle <101118943+sconkfinkle@users.noreply.github.com>
2022-10-04 14:34:41 +0530

    hardhat project
    This contains all the smart contracts of the ICO project

---
+++ hardhat-project/constants/index.js
@@ -0,0 +1,14 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xc7e7595A5c85f8d2D87A89A0F65A54E47C5ebC78";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
+
+
+// ICO Token Contract address
+
+//0xe739385D59b2fCB84c7BaA8052827d5260570B78
+
+
+//ICO Contract ABI
+
+// extract from artifacts/contracts under the hardhat project 
---
+++ hardhat-project/scripts/deploy.js
@@ -0,0 +1,46 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS } = require("../constants");
+
+async function main() {
+  // Address of the Crypto Devs NFT contract that you deployed in the previous module
+  //0xc7e7595A5c85f8d2D87A89A0F65A54E47C5ebC78
+  const cryptoDevsNFTContract = CRYPTO_DEVS_NFT_CONTRACT_ADDRESS;
+
+  /*
+    A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+    so cryptoDevsTokenContract here is a factory for instances of our CryptoDevToken contract.
+    */
+  const cryptoDevsTokenContract = await ethers.getContractFactory(
+    "CryptoDevToken"
+  );
+
+  // deploy the contract
+  const deployedCryptoDevsTokenContract = await cryptoDevsTokenContract.deploy(
+    cryptoDevsNFTContract
+  );
+
+  // print the address of the deployed contract
+  console.log(
+    "Crypto Devs Token Contract Address:",
+    deployedCryptoDevsTokenContract.address
+  );
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
+
+  // Deployed Contract address to Goerli network
+
+  //0xe739385D59b2fCB84c7BaA8052827d5260570B78
